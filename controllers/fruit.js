e09f3dae905087ef69484f871ec722f702983de4
yimd85 <yimd85@gmail.com>
2023-06-28 22:49:43 -0400

    finished fruits app and started on auth

--- controllers/fruit.js
+++ controllers/fruit.js
@@ -19,4 +19,64 @@ router.get('/new', (req, res) => {
     res.render('fruits/new.ejs')
 })
 
+
+router.post('/', async (req, res) => {
+    //example of the req.body OBJECT that comes in
+    // {
+    //     name: 'mango',
+    //     color: 'green',
+    //     readyToEat: 'on'
+    // }
+    console.log(req.body)
+    if(req.body.readyToEat === 'on'){
+        req.body.readyToEat = true;
+    }else {
+        req.body.readyToEat = false;
+    }
+    //this ternary will update the readyToEat property value similiar to how the 
+    // conditional above does it
+    // req.body.readyToEat = req.body.readyToEat === 'on' ? true : false
+    
+    //after it goes through conditional,
+    //the req.body OBJECT will look like below
+    // {
+    //     name: 'mango',
+    //     color: 'green',
+    //     readyToEat: true
+    // }
+    
+    await Fruit.create(req.body);
+    res.redirect('/fruit');
+})
+
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    const fruit = await Fruit.findById(id);
+    res.render("fruits/show.ejs", { fruit })
+})
+
+
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+    await Fruit.findByIdAndDelete(id);
+    res.redirect('/fruit')
+})
+
+
+router.get('/:id/edit', async (req, res) => {
+    const id = req.params.id;
+    const fruit = await Fruit.findById(id);
+    res.render('fruits/edit.ejs', { fruit })
+})
+
+router.put('/:id', async (req, res) => {
+    const id = req.params.id;
+    req.body.readyToEat = req.body.readyToEat === 'on' ? true : false;
+    await Fruit.findByIdAndUpdate(id, req.body);
+    res.redirect('/fruit')
+})
+
+
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const FruitRouter = require('./controllers/fruit');
+const methodOverride = require('method-override');
 const app = express();
 
 
 //middleware
-app.use(express.static("public"))
-
+app.use(express.static("public"));
+app.use(express.urlencoded()); //allows the the req.body to be read from the form
 //will have a prefix of /fruit on top of what is defined as a path on Fruitrouter
+app.use(methodOverride('_method'));
 app.use("/fruit", FruitRouter)
 
 
